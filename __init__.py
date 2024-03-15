b775869749c1a6a3e91bcc0f63190026d566bffc
JarbasAi <jarbasai@mailfence.com>
2024-03-15 03:53:18 +0000

    automodernize (untested)

--- __init__.py
+++ __init__.py
@@ -1,21 +1,20 @@
 import os
-from mycroft import MycroftSkill, intent_file_handler
+import subprocess
+from urllib.parse import quote
+
 import requests
 from bs4 import BeautifulSoup
-from mycroft.audio import wait_while_speaking, is_speaking
-from mycroft.util import get_cache_directory
-from mycroft.util import play_mp3
-from urllib.parse import quote
-import subprocess
+from ovos_utils.file_utils import get_cache_directory
+from ovos_utils.sound import play_audio as play_mp3
+from ovos_workshop.decorators import intent_handler
+from ovos_workshop.skills import OVOSSkill
 
-class TodaysGospel(MycroftSkill):
-    def __init__(self):
-        MycroftSkill.__init__(self)
 
+class TodaysGospel(OVOSSkill):
     def initialize(self):
         self.p = None
 
-    @intent_file_handler('gospel.todays.intent')
+    @intent_handler('gospel.todays.intent')
     def handle_gospel_todays(self, message):
         self.speak_dialog('gospel.todays')
         url = requests.get('https://evangeli.net/gospel')
@@ -30,10 +29,4 @@ class TodaysGospel(MycroftSkill):
         self.log.debug('Running curl {}'.format(mp3))
         args = ['curl', '-L', quote(mp3, safe=":/"), '-o', stream]
         self.curl = subprocess.Popen(args)
-        if is_speaking():
-            wait_while_speaking()
         play_mp3(stream)
-
-def create_skill():
-    return TodaysGospel()
-
